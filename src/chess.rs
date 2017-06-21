baeae613498ce1004e6b2fc2a50124058141bed2
Stian Soltvedt <soltvedt.stian@gmail.com>
2017-06-21 10:27:52 +0200

    Moved Chess code to chess.rs

---
+++ src/chess.rs
@@ -0,0 +1,117 @@
+use std;
+use std::collections::HashMap;
+
+pub struct Chess {
+    board: HashMap<String, Piece>
+}
+
+impl Chess {
+    pub fn new() -> Chess {
+        use self::PieceType::*;
+        use self::Color::*;
+        let positions = [
+            ("A1", Piece{piecetype: Rook,   color: White}), ("B1", Piece{piecetype: Knight, color: White}),
+            ("C1", Piece{piecetype: Bishop, color: White}), ("D1", Piece{piecetype: King,   color: White}),
+            ("E1", Piece{piecetype: Queen,  color: White}), ("F1", Piece{piecetype: Bishop, color: White}),
+            ("G1", Piece{piecetype: Knight, color: White}), ("H1", Piece{piecetype: Rook,   color: White}),
+            ("A2", Piece{piecetype: Pawn,   color: White}), ("B2", Piece{piecetype: Pawn,   color: White}),
+            ("C2", Piece{piecetype: Pawn,   color: White}), ("D2", Piece{piecetype: Pawn,   color: White}),
+            ("E2", Piece{piecetype: Pawn,   color: White}), ("F2", Piece{piecetype: Pawn,   color: White}),
+            ("G2", Piece{piecetype: Pawn,   color: White}), ("H2", Piece{piecetype: Pawn,   color: White}),
+            ("A8", Piece{piecetype: Rook,   color: Black}), ("B8", Piece{piecetype: Knight, color: Black}),
+            ("C8", Piece{piecetype: Bishop, color: Black}), ("D8", Piece{piecetype: King,   color: Black}),
+            ("E8", Piece{piecetype: Queen,  color: Black}), ("F8", Piece{piecetype: Bishop, color: Black}),
+            ("G8", Piece{piecetype: Knight, color: Black}), ("H8", Piece{piecetype: Rook,   color: Black}),
+            ("A7", Piece{piecetype: Pawn,   color: Black}), ("B7", Piece{piecetype: Pawn,   color: Black}),
+            ("C7", Piece{piecetype: Pawn,   color: Black}), ("D7", Piece{piecetype: Pawn,   color: Black}),
+            ("E7", Piece{piecetype: Pawn,   color: Black}), ("F7", Piece{piecetype: Pawn,   color: Black}),
+            ("G7", Piece{piecetype: Pawn,   color: Black}), ("H7", Piece{piecetype: Pawn,   color: Black})
+        ];
+
+        let mut newboard: HashMap<String, Piece> = HashMap::new();
+        for set in positions.iter() {
+            newboard.insert(String::from(set.0), set.1);
+        }
+
+        Chess { board: newboard }
+    }
+
+    pub fn perform_move(&mut self, move_to_perform: &str) -> Result<(), &'static str> {
+        if !self.is_valid_move(move_to_perform) {
+            return Result::Err("Invalid move");
+        }
+
+        let (from, to) = move_to_perform.split_at(2);
+        let piece = self.board.get(from).unwrap().clone();
+        self.board.insert(String::from(to), piece);
+        self.board.remove(from);
+        Ok(())
+    }
+
+    fn is_valid_move(&self, move_to_perform: &str) -> bool {
+        // TODO: Check rules
+        move_to_perform.len() == 4
+    }
+}
+
+#[allow(unused_variables)]
+impl std::fmt::Display for Chess {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+        for x in 1..9 {
+            print!("{} ", x);
+            for letter in letters.iter() {
+                print!("{} ", match self.board.get(&format!("{}{}", letter, x)) {
+                    Some(t) => t.letter(),
+                    None => "."
+                });
+            }
+            println!();
+        }
+        println!("  A B C D E F G H");
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Piece {
+    pub color: Color,
+    pub piecetype: PieceType
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Color {
+    Black,
+    White    
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PieceType {
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn
+}
+
+impl Piece {
+    fn letter(&self) -> &'static str {
+        use self::PieceType::*;
+        use self::Color::*;
+        match (self.piecetype, self.color) {
+            (King,   White) => "♚",
+            (Queen,  White) => "♛",
+            (Bishop, White) => "♝",
+            (Knight, White) => "♞",
+            (Rook,   White) => "♜",
+            (Pawn,   White) => "♟",
+            (King,   Black) => "♔",
+            (Queen,  Black) => "♕",
+            (Bishop, Black) => "♗",
+            (Knight, Black) => "♘",
+            (Rook,   Black) => "♖",
+            (Pawn,   Black) => "♙"
+        }
+    }
+}
