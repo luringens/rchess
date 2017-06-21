bc1b479c13139ec674bcf3fcb0d18a5a2f19985f
Stian Soltvedt <soltvedt.stian@gmail.com>
2017-06-21 10:36:41 +0200

    Rewrite welcome message

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,25 @@ use std::io;
 mod chess;
 
 fn main() {
-    println!("Welcome to rchess!");
+    let clear = format!("{}[2J", 27 as char);
     let mut game = chess::Chess::new();
-
+    
+    println!("Welcome to rchess!");
+    println!("Enter the field to move from and to in the format 'A2A4'.");
+    println!("Type 'quit' to stop playing.");
+    
     let mut running = true;
     while running {
         println!("{}", game);
-        print!("{}[2J", 27 as char); // Clear screen
-
+        
         let mut input = String::new();
         io::stdin().read_line(&mut input)
             .expect("Failed to read line");
         let input = input.trim();
 
+        print!("{}", clear);
         match input.as_ref() {
-            "exit" => running = false,
+            "quit" => running = false,
             _ => match game.perform_move(&input) {
                 Err(a) => println!("{}", a),
                 Ok(_) => println!("Done!")
